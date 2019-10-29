c8f4c6676c9a6f92413e658e9ba1ab97addf62c7
Calvin Spealman <ironfroggy@gmail.com>
2019-10-28 23:02:37 -0400

    Reworked zip exclusions to build more consistent archives

---
+++ CHANGES.md
@@ -0,0 +1,11 @@
+# Changelog
+
+### 0.5.0
+- Reworked zip exclusion list to control matches better
+- Added build exe to clean, not just cpython prep
+- Small improvements to arch handling, will add flag soon
+- Re-enabled compressed builds
+- Moved dep install to the prep CPython after copy, not before
+- Fixed a bug in unpack and install on first launch, was running on every launch
+- Created a flag file `requirements_installed.txt` to indicate completed installation
+- Automated the build step in a new command, `python`, including checkout
--- feetmaker.py
+++ feetmaker.py
@@ -26,21 +26,23 @@ setup_parser = subparsers.add_parser('setup')
 
 python_parser = subparsers.add_parser('python')
 
+arch = "amd64" # win32 or amd64
 python_loc_default = "cpython"
 python_loc = os.getenv("FEET_PYTHON_DIR", python_loc_default)
-
-arch = "amd64" # win32 or amd64
 py_bin = os.path.join(python_loc, 'PCBuild', arch, 'python.exe')
 
 # These patterns will be excluded from the generated Zip archives
 zip_excludes = [
-    '*__pycache__*',
-    '*.pyc',
+    '**/__pycache__',
+    '**/*.pyc',
+    '**/*.pdb',
+    '**/*.exp',
     '*venv*.exe',
-    '*/pythonw.exe',
-    '*/py.exe',
-    '*/pyw.exe',
-    'test/**',
+    '**/pythonw.exe',
+    '**/_test*',
+    '**/py.exe',
+    '**/pyw.exe',
+    '*test_*',
 ]
 
 # These third-party packages will be included in the build
@@ -81,6 +83,29 @@ non_zip_modules = (
 def clean():
     if os.path.exists("feet/cpython"):
         shutil.rmtree("feet/cpython")
+    for name in os.listdir('build'):
+        path = os.path.join('build', name)
+        if os.path.isdir(path):
+            shutil.rmtree(path)
+        else:
+            os.unlink(path)
+
+
+def is_excluded(name):
+    for pattern in zip_excludes:
+        if fnmatch.fnmatch(name, pattern):
+            return True
+    return False
+
+
+def ignore_excludes(dirname, names):
+    if is_excluded(dirname):
+        print("IGNORE", dirname)
+        return []
+    return [
+        name for name in names
+        if is_excluded(os.path.join(dirname, name))
+    ]
 
 
 def zipdir(path, relto, dest, compression):
@@ -96,12 +121,7 @@ def zipdir(path, relto, dest, compression):
         for file in files:
             src = os.path.join(root, file)
             name = os.path.relpath(src, relto)
-            excluded = False
-            for pattern in zip_excludes:
-                if fnmatch.fnmatch(name, pattern):
-                    excluded = True
-                    break
-            if not excluded:
+            if not is_excluded(name):
                 zipf.write(src, name)
     
     zipf.close()
@@ -122,8 +142,12 @@ def main():
             subprocess.check_call(f"git checkout 3.7")
 
         assert os.path.exists(f"./{python_loc}/PCBuild/build.bat")
-        print(f"./{python_loc}/PCBuild/build.bat -c Release -p x64 -t Build")
-        subprocess.check_call(f"{python_loc}\\PCBuild\\build.bat -c Release -p x64 -t Build")
+        if arch == "amd64":
+            p = "x64"
+        else:
+            p = arch
+        print(f"./{python_loc}/PCBuild/build.bat -c Release -p {p} -t Build")
+        subprocess.check_call(f"{python_loc}\\PCBuild\\build.bat -c Release -p {p} -t Build")
 
     elif not args.command or args.command == "build":
         clean()
@@ -135,14 +159,15 @@ def main():
         shutil.copytree(
             os.path.join(python_loc, "PCbuild", arch),
             "feet/cpython",
-            ignore=shutil.ignore_patterns('*.pdb'),
+            ignore=ignore_excludes,
         )
         
         os.makedirs("feet/cpython/lib")
 
         for name in os.listdir(os.path.join(python_loc, 'Lib')):
-            if name in non_zip_modules:
-                src = os.path.join(python_loc, "lib", name)
+
+            src = os.path.join(python_loc, "lib", name)
+            if os.path.splitext(name)[0] in non_zip_modules:
                 if not os.path.exists(src):
                     name += ".py"
                     src = os.path.join(python_loc, "lib", name)
@@ -151,44 +176,41 @@ def main():
                 else:
                     print(f"Copying {src}")
                     if os.path.isdir(src):
-                        shutil.copytree(src, f"feet/cpython/lib/{name}")
+                        shutil.copytree(src, f"feet/cpython/{name}")
                     else:
                         shutil.copy(
                             src,
-                            "feet/cpython/lib/",
+                            "feet/cpython/",
                         )
-            else:
-                # TODO: are we putting things in both the zip and outside the zip? wasting space?
-                src = os.path.join('cpython', 'Lib', name)
-                dest = os.path.join("feet/cpython/lib/", name)
-                if not os.path.exists(dest):
-                    if os.path.isdir(src):
-                        shutil.copytree(src, dest)
-                    else:
-                        shutil.copyfile(src, dest)
-                else:
-                    logger.warn(f"Cannot copy {src} -> {dest}")
-        
+
         # Create the stdlib zip to make unpacking faster
         zipdir(
             os.path.join(python_loc, "lib"),
             None,
             os.path.join("feet", "cpython", "python37.zip"),
-            zipfile.ZIP_STORED,
+            zipfile.ZIP_DEFLATED,
         )
 
         # Create the archive to attach to feet.exe to self-extract
-        subprocess.check_call([py_bin, '-m', 'ensurepip'])
+        feet_py = "feet/cpython/python.exe"
+        subprocess.check_call([feet_py, '-m', 'ensurepip'])
         for name in py_deps:
             try:
-                subprocess.check_call([py_bin, '-m', 'pip', 'install', '-U', name])
+                p = subprocess.run([feet_py, '-m', 'pip', 'install', '-U', name], text=True)
+                print(f"Installing package '{name}'")
+                if p.stdout:
+                    print(p.stdout)
             except FileNotFoundError:
-                logger.error(f"Could not find python executable to install deps: {py_bin}")
+                logger.error(f"Could not find python executable to install deps: {feet_py}")
                 raise
+
+        # # Copy installed packages
         # shutil.copytree(
         #     os.path.join(python_loc, "Lib", "site-packages"),
         #     f"feet/cpython/lib/site-packages",
-        # )
+        # # )
+
+        # Create archive to attach to runtime
         zipdir(
             './feet/',
             '.',
